a2fdbe2f768bf0e026d98478ccc7517d4c3e45b0
George Rodgers <george.rodgers.chem@gmail.com>
2024-09-26 11:29:29 +0100

    update

--- script.js
+++ script.js
@@ -1,4 +1,52 @@
 // Write your code here
+
+// solution for initial problem and first stretch 
+
+// const lightbulb1 = document.querySelector(`#lightbulb1`);
+// const lightbulb2 = document.querySelector(`#lightbulb2`);
+// const lightbulb3 = document.querySelector(`#lightbulb3`);
+// const subtitle = document.querySelector(`.subtitle`);
+
+// let count = 0;
+
+// lightbulb1.addEventListener(`click`, function(){
+//     count++;
+//     if (count === 1){
+//         subtitle.innerHTML = `You've clicked the lights ${count} time`;
+//         lightbulb1.classList.toggle(`active`);
+//     } else{
+//         subtitle.innerHTML = `You've clicked the lights ${count} times`;
+//         lightbulb1.classList.toggle(`active`);
+//     }
+// });
+
+// lightbulb2.addEventListener(`click`, function(){
+//     count++;
+//     if (count === 1){
+//         subtitle.innerHTML = `You've clicked the lights ${count} time`;
+//         lightbulb2.classList.toggle(`active`);
+//     } else{
+//         subtitle.innerHTML = `You've clicked the lights ${count} times`;
+//         lightbulb2.classList.toggle(`active`);
+//     }
+// });
+
+// lightbulb3.addEventListener(`click`, function(){
+//     count++;
+//     if (count === 1){
+//         subtitle.innerHTML = `You've clicked the lights ${count} time`;
+//         lightbulb3.classList.toggle(`active`);
+//     } else{
+//         subtitle.innerHTML = `You've clicked the lights ${count} times`;
+//         lightbulb3.classList.toggle(`active`);
+//     }
+// });
+
+
+// Solution to stretch problem two
+// removed active class from lightbulb1 in index.html for this exercise
+// Instead of `click`, `mouseenter` and `mouseleave` trigger the event when mouse enteres and leaves the object
+
 const lightbulb1 = document.querySelector(`#lightbulb1`);
 const lightbulb2 = document.querySelector(`#lightbulb2`);
 const lightbulb3 = document.querySelector(`#lightbulb3`);
@@ -6,7 +54,7 @@ const subtitle = document.querySelector(`.subtitle`);
 
 let count = 0;
 
-lightbulb1.addEventListener(`click`, function(){
+lightbulb1.addEventListener((`mouseenter`), function(){
     count++;
     if (count === 1){
         subtitle.innerHTML = `You've clicked the lights ${count} time`;
@@ -17,7 +65,17 @@ lightbulb1.addEventListener(`click`, function(){
     }
 });
 
-lightbulb2.addEventListener(`click`, function(){
+lightbulb1.addEventListener((`mouseleave`), function(){
+    if (count === 1){
+        subtitle.innerHTML = `You've clicked the lights ${count} time`;
+        lightbulb1.classList.toggle(`active`);
+    } else{
+        subtitle.innerHTML = `You've clicked the lights ${count} times`;
+        lightbulb1.classList.toggle(`active`);
+    }
+});
+
+lightbulb2.addEventListener((`mouseenter`), function(){
     count++;
     if (count === 1){
         subtitle.innerHTML = `You've clicked the lights ${count} time`;
@@ -28,8 +86,28 @@ lightbulb2.addEventListener(`click`, function(){
     }
 });
 
-lightbulb3.addEventListener(`click`, function(){
+lightbulb2.addEventListener((`mouseleave`), function(){
+    if (count === 1){
+        subtitle.innerHTML = `You've clicked the lights ${count} time`;
+        lightbulb2.classList.toggle(`active`);
+    } else{
+        subtitle.innerHTML = `You've clicked the lights ${count} times`;
+        lightbulb2.classList.toggle(`active`);
+    }
+});
+
+lightbulb3.addEventListener((`mouseenter`), function(){
     count++;
+    if (count === 1){
+        subtitle.innerHTML = `You've clicked the lights ${count} time`;
+        lightbulb3.classList.toggle(`active`);
+    } else{
+        subtitle.innerHTML = `You've clicked the lights ${count} times`;
+        lightbulb3.classList.toggle(`active`);
+    }
+});
+
+lightbulb3.addEventListener((`mouseleave`), function(){
     if (count === 1){
         subtitle.innerHTML = `You've clicked the lights ${count} time`;
         lightbulb3.classList.toggle(`active`);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+const lightbulb1 = document.querySelector(`#lightbulb1`);
+// const lightbulb2 = document.querySelector(`#lightbulb2`);
+// const lightbulb3 = document.querySelector(`#lightbulb3`);
+// const subtitle = document.querySelector(`.subtitle`);
